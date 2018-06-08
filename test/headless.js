5ef403b7b7a19aaa147f788e5191cca2ef86e663
Madeleine Marcano <madeleine1302@gmail.com>
2018-06-08 16:05:38 -0400

    Subiendo todos los archivos e instalaciones nuevas

--- test/headless.js
+++ test/headless.js
@@ -1,9 +1,10 @@
 global.window = global;
 global.assert = require('chai').assert;
+/*global.require = require('chai').*/
 global.fixtures = {
-  cohorts: require('../data/cohorts.json'),
-  progress: require('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
-  users: require('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
+    cohorts: require('../data/cohorts.json'),
+    progress: require('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
+    users: require('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
 };
 require('../src/data');
 require('./data.spec.js');
