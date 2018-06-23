6304fcc8e75d60e307a92a9ac3848f2d9a16cdbb
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-06-23 03:01:14 -0400

    estoy tratando de llamar los 3 json

--- src/asserts/js/data.js
+++ src/asserts/js/data.js
@@ -1,4 +1,4 @@
-document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
+ /*document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
 
 function cargarJSON() {
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
@@ -17,14 +17,7 @@ function cargarJSON() {
         <td>${users.id}</td>
         </tr>`
 
-        /*
-        <div class="col-sm-3"><div class="data-list">${users.id}</div></div>
-        <div class="col-sm-3"><div class="data-list">${users.signupCohort} </div></div>
-        <div class="col-sm-3"><div class="data-list">${users.name}</div></div>
-        <div class="col-sm-3"><div class="data-list">${users.timezone}</div></div>
-        <hr class="hr">*/   
-     
-        
+
         ;
         console.log(data);
       });
@@ -34,5 +27,52 @@ function cargarJSON() {
       console.log(error);
     });
 };
+*/
 
+Promise.all([ // Ejecuta todas las llamadas de manera paralela
+  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
+  fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
+  fetch('../data/cohorts.json')
+]).then((responses)=>{ // Responde a todas las promesas
+  return Promise.all(responses.map((response => response.json())));
+
+}).then((responseJsons)=>{ // Arreglo de respuestas en json
+  const users = responseJsons[0];
+  const progress = responseJsons[1];
+  const cohorts = responseJsons[2];
+  console.log(users);
+  console.log(progress);
+  console.log(cohorts);
+  users.forEach(element => {
+    console.log(element.name);
+    console.log(element.role);
+    console.log(element.id);
+
+
+  
+
+  })
+  
+  
+  progress.forEach(element2 => {
+    console.log(element2.intro);
+    console.log(element2.role);
+    console.log(element2.id);
+
+
+  
+
+  });
+
+
+// hacer for o forEach para seleccionar un id o name, lo que sea...
+ 
+        
+  //
+  // Código que ocupa los jsons...
+  //
+}).catch(
+  (error)=>{ // Al menos una llamada falló
+  }
+);
 
