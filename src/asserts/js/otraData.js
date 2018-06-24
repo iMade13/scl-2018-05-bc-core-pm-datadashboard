cbc4f18b7628204ec503412b5ad6ad3031dacc81
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-06-24 16:27:51 -0400

    tratando de mostrar a info de progress pero no funciona

--- src/asserts/js/otraData.js
+++ src/asserts/js/otraData.js
@@ -1,7 +1,3 @@
-
-
-
-
 Promise.all([ // Ejecuta todas las llamadas de manera paralela
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
@@ -12,12 +8,10 @@ Promise.all([ // Ejecuta todas las llamadas de manera paralela
 
 }).then((responseJsons)=>{ // Arreglo de respuestas en json
   const users = responseJsons[0];
-  const progress = responseJsons[1];
-  const cohorts = responseJsons[2];
+
 
   console.log(users);
-  console.log(progress);
-  console.log(cohorts);
+
 
   users.forEach(element => { // muestra la data de users en la consola del navegador
     console.log(element.name);
@@ -25,35 +19,27 @@ Promise.all([ // Ejecuta todas las llamadas de manera paralela
     console.log(element.id);
     console.log(element.timezone);
 
-    progress.forEach(progressElement => { // muestra la data de progress en la consola del navegador
-        console.log(progressElement.intro);
-        console.log(progressElement.totalDuration);
-
-  })
-  
-  .then(function(data) { //esta funcion intenta mostrar parte de la data en html
-    let html = '';
-    data.forEach(function(users) {
-      html += 
-      `<tr>
-      <th scope="row">${users.name}</th>
-      <td>${users.role}</td>
-      <td>${users.timezone}</td>
-      <td>${users.id}</td>
-      </tr>`;   
-      console.log(data);
-    });
-    document.getElementById('resultado').innerHTML = html;
   })
 
-// hacer for o forEach para seleccionar un id o name, lo que sea...
- 
-        
-  //
-  // Código que ocupa los jsons...
-  //
+  }).then((responseJsons2)=>{ // Arreglo de respuestas en json
+
+  const progress = responseJsons2[1];
+
+
+
+  console.log(progress);
+
+
+  progress.forEach(element2 => { // muestra la data de users en la consola del navegador
+    console.log(element2.intro)
+
+  })
+
 }).catch(
   (error)=>{ // Al menos una llamada falló
   }
-);})
+);
+
+
+
 
