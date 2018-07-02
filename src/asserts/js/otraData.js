51aade57c2a455a259bba232da722fad137e9e05
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-07-02 02:20:33 -0400

    pequeño avance experimental, mientras trato de hacer un for

--- src/asserts/js/otraData.js
+++ src/asserts/js/otraData.js
@@ -1,26 +1,53 @@
+let users;
+let progress;
+let cohorts ;
+
+let percentGral;
+
+let exercises;
+let exercisesCompleted;
+let exercisesPercent;
+
+let quiz;
+let quizCompleted;
+let quizPercent;
+
+let reads;
+let readsCompleted;
+let readsPercent;
+
 // archivo para experimentar con js
 Promise.all([ // Ejecuta todas las llamadas de manera paralela.
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
   fetch('../data/cohorts.json')
-]).then((responses)=>{ // Responde a todas las promesas.
+]).then((responses) => { // Responde a todas las promesas.
   return Promise.all(responses.map((response => response.json())));
-}).then((data)=>{ // Arreglo de respuestas en json.
-  
-  let users = data[0];
-  let progress = Object.values(data[1]);
-  let cohorts = Object.entries(data[2]);
+}).then((data) => { // Arreglo de respuestas en json.
+  users = data[0];
+  progress = Object.values(data[1]);
+  cohorts = Object.entries(data[2]);
 
-  console.log(cohorts)
-  console.log(progress)
-  console.log(users)
-
-
-
-
+  console.log(cohorts);
+  console.log(progress);
+  console.log(users);
 
 }).catch(
-  (error)=>{ // Al menos una llamada falló.
+  () => {
     console.log('fallo fetch');
   }
 );
+
+window.computeUsersStats (users, progress, courses); {
+
+  let User = []; 
+
+  for (let i = 0; i < users.length; i++){
+
+    let idUser = users[i].id;
+
+  }
+}
+
+
+
