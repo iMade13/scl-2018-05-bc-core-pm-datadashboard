ef7b40bbd5b99f34f02f52ae219026706638dee3
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-07-01 20:45:46 -0400

    se crean archivos experimentales, otraData.js devuelve arreglos

--- src/asserts/js/otraData.js
+++ src/asserts/js/otraData.js
@@ -1,35 +1,26 @@
 // archivo para experimentar con js
-
-Promise.all([ // Ejecuta todas las llamadas de manera paralela
+Promise.all([ // Ejecuta todas las llamadas de manera paralela.
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
   fetch('../data/cohorts.json')
+]).then((responses)=>{ // Responde a todas las promesas.
+  return Promise.all(responses.map((response => response.json())));
+}).then((data)=>{ // Arreglo de respuestas en json.
+  
+  let users = data[0];
+  let progress = Object.values(data[1]);
+  let cohorts = Object.entries(data[2]);
 
-]).then((responses)=>{ // Responde a todas las promesas
-  return Promise.all(responses.map((response => response.json()))); // crea un nuevo array
-}).then((responseJsons)=>{ // Arreglo de respuestas en json
-  const users = responseJsons[0];
-  const cohorts = responseJsons[1];
-  const progress = responseJsons[2];
-
- /* console.log(users);
-  console.log(progress);
-  console.log(cohorts);*/
-
-  users.forEach(element => { // muestra la data de users en la consola del navegador
-    console.log(element.name);
-    console.log(element.role);
-    console.log(element.id);
-    console.log(element.timezone);
-;
-  })
+  console.log(cohorts)
+  console.log(progress)
+  console.log(users)
 
 
 
-console.log(result);
+
+
 }).catch(
-  (error)=>{ // Al menos una llamada falló
+  (error)=>{ // Al menos una llamada falló.
+    console.log('fallo fetch');
   }
 );
-
-
